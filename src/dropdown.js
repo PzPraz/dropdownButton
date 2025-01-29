db51a3ae8a65d1755ca623ae8ac7643d88b6f136
pzpraz <95980337+PzPraz@users.noreply.github.com>
2025-01-29 23:44:56 +0700

    dropdown button published

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,9 +1,13 @@
 import "./dropdown.css";
 
-const dropDownButton = document.querySelector(".dropbtn");
-const dropDownContent = document.querySelector(".dropdown-content");
+const initDropdown = function () {
+  const dropDownButton = document.querySelector(".dropbtn");
+  const dropDownContent = document.querySelector(".dropdown-content");
 
-dropDownButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  dropDownContent.classList.toggle("visible");
-});
+  dropDownButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    dropDownContent.classList.toggle("visible");
+  });
+};
+
+export { initDropdown };
